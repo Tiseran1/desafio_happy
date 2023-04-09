e02823732490f667f3b3cddc2f5a8e71a99bb3fa
Camilo <crojas.historia@gmail.com>
2023-04-09 19:11:51 -0400

    segundo

--- src/components/contacto/index.js
+++ src/components/contacto/index.js
@@ -5,10 +5,12 @@ export default () => {
     return (
         <div className="contacto-class"> 
             <h2> Qué tipo de hacha buscas? </h2>
+            <p>Correo</p>
             <input
                 className="form-control me-2 my-3"
                 placeholder="Correo" 
             />
+            <p>Descripción</p>
             <input
                 className="form-control me-2 my-3"
                 placeholder="Uso que ke dsarás del hacha " 
